c5783aea90f4b35180d70cd379d6ee710cbf05b2
Michael Ingleton <mingleton42@gmail.com>
2019-05-22 16:54:07 -0600

    updated package-lock.json

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import profilePicture from "../../../static/assets/images/about/profilepicture.jpg"
+import profilePicture from "../../../static/assets/images/about/profilepicture.jpg";
 
 export default function() {
     return (
